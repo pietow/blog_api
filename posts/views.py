ef5a77fc3ff3807a35d55d9771e555b4a3893b9d
piet <pprotassow@posteo.de>
2023-11-02 17:39:12 +0100

    CRUD Blog

---
+++ posts/views.py
@@ -0,0 +1,14 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Post
+from .serializers import PostSerializer
+
+class PostList(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+
